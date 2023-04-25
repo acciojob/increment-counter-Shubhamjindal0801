1d51aea3faf9b1a96859f921321248cc229ac8ca
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-26 00:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0;
+let pTag = document.getElementById("counter")
+document.getElementById("incrementBtn").addEventListener("click",()=>{
+	alert(count);
+	count++
+	counter.innerText=count
+})
